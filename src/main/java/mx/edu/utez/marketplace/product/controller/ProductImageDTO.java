bcc04daa5d05765dc164eb17f66b99f7c8c66250
DiegoAraithVillalobosHernandez <20203tn063@utez.edu.mx>
2022-02-22 14:04:25 -0600

    3

---
+++ src/main/java/mx/edu/utez/marketplace/product/controller/ProductImageDTO.java
@@ -0,0 +1,52 @@
+package mx.edu.utez.marketplace.product.controller;
+
+import java.util.Base64;
+
+//Se modifica el get devuelve un byte[]
+public class ProductImageDTO {
+    private long id;
+    private String name;
+    //No recibe un byte[] sino un String
+    private String fileBase64;
+    //No recibe un producto, eso se hace en productImage
+
+
+    public ProductImageDTO() {
+    }
+
+    public ProductImageDTO(String name, String fileBase64) {
+        this.name = name;
+        this.fileBase64 = fileBase64;
+    }
+
+    public ProductImageDTO(long id, String name, String fileBase64) {
+        this.id = id;
+        this.name = name;
+        this.fileBase64 = fileBase64;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Se decodifica el archivo
+    public byte[] getFileBase64() {
+        return Base64.getDecoder().decode(fileBase64.replace(" ","+"));
+    }
+
+    public void setFileBase64(String fileBase64) {
+        this.fileBase64 = fileBase64;
+    }
+}
---
+++ src/main/java/mx/edu/utez/marketplace/product/controller/ProductService.java
@@ -0,0 +1,75 @@
+package mx.edu.utez.marketplace.product.controller;
+
+import mx.edu.utez.marketplace.product.model.Product;
+import mx.edu.utez.marketplace.product.model.ProductImage;
+import mx.edu.utez.marketplace.product.model.ProductRepository;
+import mx.edu.utez.marketplace.utils.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+//Las clases de Servicio nos sirven para llevar la lógica del negocio
+// Es decir los datos recibidos son tratados aquí
+@Service
+@Transactional//Debe ser de Spring
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Message> findAll(){
+        return new ResponseEntity<>(new Message("ok", false, productRepository.findAll()),
+                HttpStatus.OK);
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Message> findById(long id){
+        if (productRepository.existsById(id)) {
+            return  new ResponseEntity<>(new Message("ok", false, productRepository.findById(id)),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Message("El producto no existe", true, null),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @Transactional(rollbackFor = {SQLException.class})
+    public ResponseEntity<Message> save(Product product){
+        //No sabemos el id porque es automatico, por ende solo podemos buscar por nombre
+        Optional<Product> exists = productRepository.findByName(product.getName());
+
+        if (exists.isPresent()){
+            return new ResponseEntity<>(new Message("Producto existente", true, null),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+
+        List<ProductImage> images = product.getImages();
+        //Obtenemos el id del producto temporal
+        Product idProduct = productRepository.saveAndFlush(product);
+        //Seteamos a cada imagen el id del producto
+        images.forEach(image -> image.setProduct(idProduct));
+
+        idProduct.setImages(images);
+        Product savedProduct = productRepository.saveAndFlush(idProduct);
+
+        return new ResponseEntity<>(new Message("ok", false, productRepository.saveAndFlush(savedProduct)),
+                HttpStatus.OK);
+    }
+
+    @Transactional
+    public ResponseEntity<Message> update(Product product){
+        //Necesitamos verificar que exista dicho producto a modificar
+        if (productRepository.existsById(product.getId())){
+            return new ResponseEntity<>(new Message("ok", false, productRepository.saveAndFlush(product)),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Message("Este producto na existe", true, null),
+                HttpStatus.BAD_REQUEST);
+    }
+}
